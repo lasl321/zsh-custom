e9531208b77eff52b9e468950bc130ee646ee75b
Luis Sotomayor <luis.sotomayor@stansonhealth.com>
2019-02-26 10:21:03 -0800

    Updated aliases

--- custom/custom-004-aliases.zsh
+++ custom/custom-004-aliases.zsh
@@ -1,12 +1,15 @@
-alias zshconfig="code ~/.oh-my-zsh/"
+alias zshcustom="code ~/source/lasl321/zsh-custom/"
 
 alias notes="code -n ~/Google\ Drive/notes/"
 
-alias xrunapplication="pushd ~/source/stanson && ./grailsw clean && GRAILS_OPTS=\"-server -Xmx3G -Xms2G -Dfile.encoding=UTF-8\" ./grailsw run-war"
-alias xbuildapplication="pushd ~/source/stanson && ./grailsw compile -clover.on --non-interactive -verbose"
+alias ssra="pushd ~/source/stanson && ./grailsw run-app"
+alias ssrw="pushd ~/source/stanson && ./grailsw run-war"
+alias ssc="pushd ~/source/stanson && ./grailsw compile"
+alias sscc="pushd ~/source/stanson && ./grailsw compile -clover.on --non-interactive -verbose"
+alias sscl="pushd ~/source/stanson && rm -rf target/classes target/clover target/test-classes target/test-reports target/coverage test-output out tomcat.8080/"
+
 alias xloginapplication="curl http://localhost:8080/stanson/j_spring_security_check -v -d 'j_username=dev&j_password=dev' -c cookies.txt"
 alias xcurl="curl -b cookies.txt -H 'Accept: application/json'"
-alias xcleanapplication="rm -rf target/classes target/clover target/test-classes target/test-reports target/coverage test-output out tomcat.8080/"
 
 alias gdt='git difftool --no-prompt'
 alias gpf='git push --force'
