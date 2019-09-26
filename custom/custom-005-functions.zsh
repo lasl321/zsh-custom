bddb95773fc84366e5b575ade769f736547c359e
Luis Sotomayor <Luis_Sotomayor@premierinc.com>
2019-09-25 18:38:05 -0700

    Updates to local DB

--- custom/custom-005-functions.zsh
+++ custom/custom-005-functions.zsh
@@ -14,3 +14,16 @@ function pr () {
         echo "No file found at '$filename'"
     fi
 }
+
+function copy-for-build-dev-data () {
+    rm -rf ~/source/docker-private-containers/build-dev-data/stanson 
+    mkdir ~/source/docker-private-containers/build-dev-data/stanson
+
+    cp -R ~/source/stanson/db ~/source/docker-private-containers/build-dev-data/stanson/db
+    cp -R ~/source/stanson/migrations ~/source/docker-private-containers/build-dev-data/stanson/migrations
+    cp -R ~/source/stanson/gradle ~/source/docker-private-containers/build-dev-data/stanson/gradle
+    cp -R ~/source/stanson/buildSrc ~/source/docker-private-containers/build-dev-data/stanson/buildSrc
+    cp ~/source/stanson/build.gradle ~/source/docker-private-containers/build-dev-data/stanson/
+    cp ~/source/stanson/gradlew ~/source/docker-private-containers/build-dev-data/stanson/gradlew
+    cp ~/source/stanson/security-inspections.gradle ~/source/docker-private-containers/build-dev-data/stanson/
+}
