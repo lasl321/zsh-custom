6c6478df6cdd9a23a4bd8bee2926f37946375c63
Luis Sotomayor <Luis_Sotomayor@premierinc.com>
2020-03-18 10:04:34 -0700

    Removed functions, added personal Chrome

--- custom/custom-005-functions.zsh
+++ custom/custom-005-functions.zsh
@@ -1,20 +1,3 @@
-function pr-file-name () {
-    echo "~/Google\ Drive/Documentation/Pull\ Requests/$(git symbolic-ref --short -q HEAD | sed 's/\//-/g').md"
-}
-
-function prf () {
-    code -n "$(pr-file-name)"
-}
-
-function pr () {
-    filename="$(pr-file-name)"
-    if [[ -f $filename ]]; then
-        hub pull-request -o -F $filename
-    else
-        echo "No file found at '$filename'"
-    fi
-}
-
 function copy-for-build-dev-data () {
     rm -rf ~/source/docker-private-containers/build-dev-data/stanson 
     mkdir ~/source/docker-private-containers/build-dev-data/stanson
@@ -26,4 +9,8 @@ function copy-for-build-dev-data () {
     cp ~/source/stanson/build.gradle ~/source/docker-private-containers/build-dev-data/stanson/
     cp ~/source/stanson/gradlew ~/source/docker-private-containers/build-dev-data/stanson/gradlew
     cp ~/source/stanson/security-inspections.gradle ~/source/docker-private-containers/build-dev-data/stanson/
+}
+
+function my-chrome () {
+    /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --user-data-dir='${TMPDIR}/my-chrome' --no-proxy-server
 }
