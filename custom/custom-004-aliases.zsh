ce04d7cec9a82a0fc881ad61ad2196c8f10add7d
Luis Sotomayor <Luis_Sotomayor@premierinc.com>
2020-03-18 10:10:29 -0700

    Updated aliases; don't need to switch directories

--- custom/custom-004-aliases.zsh
+++ custom/custom-004-aliases.zsh
@@ -2,11 +2,11 @@ alias zshcustom="code ~/source/lasl321/zsh-custom/"
 
 alias notes="code -n ~/Google\ Drive/notes/"
 
-alias ssra="pushd ~/source/stanson && ./grailsw run-app"
-alias ssrw="pushd ~/source/stanson && ./grailsw run-war"
-alias ssc="pushd ~/source/stanson && ./grailsw compile"
-alias sscc="pushd ~/source/stanson && ./grailsw compile -clover.on --non-interactive -verbose"
-alias sscl="pushd ~/source/stanson && rm -rf target/classes target/clover target/test-classes target/test-reports target/coverage test-output out tomcat.8080/"
+alias ssra="./grailsw run-app"
+alias ssrw="./grailsw run-war"
+alias ssc="./grailsw compile"
+alias sscc="./grailsw compile -clover.on --non-interactive -verbose"
+alias sscl="rm -rf target/classes target/clover target/test-classes target/test-reports target/coverage test-output out tomcat.8080/"
 
 alias xloginapplication="curl http://localhost:8080/stanson/j_spring_security_check -v -d 'j_username=dev&j_password=dev' -c cookies.txt"
 alias xcurl="curl -b cookies.txt -H 'Accept: application/json'"
