2982f8a2684cccb93cf7f27dbef310fcda9e7269
Luis Sotomayor <Luis_Sotomayor@premierinc.com>
2021-03-17 22:20:54 -0700

    Update to include virtualenv indication

--- custom/custom-006-variables.zsh
+++ custom/custom-006-variables.zsh
@@ -1,6 +1,7 @@
 POWERLEVEL9K_MODE='awesome-patched'
+POWERLEVEL9K_PYTHON_ICON='\U1F40D'
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon dir vcs)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs time battery)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(virtualenv status root_indicator background_jobs time battery)
 POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
 POWERLEVEL9K_SHORTEN_DELIMITER=""
 POWERLEVEL9K_SHORTEN_STRATEGY="truncate_from_right"
--- custom/custom-008-path.zsh
+++ custom/custom-008-path.zsh
@@ -1,1 +1,3 @@
 export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:/usr/local/lib/ruby/gems/2.6.0/bin:$PATH"
+export PATH="$PATH:$HOME/Library/Python/3.8/bin"
+export PATH="$PATH:$HOME/.local/bin"
