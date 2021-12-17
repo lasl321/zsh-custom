966caaf01e698c6a06c73ec1e488480ecd515c9f
Luis Sotomayor <luis@zuub.com>
2021-12-16 22:34:31 -0800

    Added npm aliases

--- custom/custom-002-aliases.zsh
+++ custom/custom-002-aliases.zsh
@@ -2,4 +2,7 @@ alias gdt='git difftool --no-prompt'
 alias gpf='git push --force'
 alias gbsc='git branch --sort=-committerdate'
 
+alias npmb='npm run build'
+alias npmbb='npm ci && npm run build'
+
 alias weather='curl https://wttr.in/Sherman_Oaks'
