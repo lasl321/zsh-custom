08ac1ab9b1b654f0d8a62fda45b194e94c085199
Luis Sotomayor <luis@zuub.com>
2022-01-08 13:40:24 -0800

    Update to show full branch name; added aliases for NPM

--- custom/custom-002-aliases.zsh
+++ custom/custom-002-aliases.zsh
@@ -4,5 +4,8 @@ alias gbsc='git branch --sort=-committerdate'
 
 alias npmb='npm run build'
 alias npmbb='npm ci && npm run build'
+alias npml='npm run lint'
+alias npmtc='npm run test:coverage'
+alias npmtw='npm run test:watch'
 
 alias weather='curl https://wttr.in/Sherman_Oaks'
