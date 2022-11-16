da3ed236072421798d2134d63fff0bfd932df370
Luis Sotomayor <luis@zuub.com>
2022-11-15 19:56:06 -0800

    feat: update alias

--- custom/custom-002-aliases.zsh
+++ custom/custom-002-aliases.zsh
@@ -1,7 +1,7 @@
 alias gdtl='git difftool --no-prompt'
 alias gpf='git push --force'
 alias gbsc='git branch --sort=-committerdate'
-alias gbDD='git branch --no-color --merged | command grep -vE "^([+*]|\s*($(git_main_branch)|$(git_develop_branch))\s*$)" | command xargs git branch -D 2>/dev/null'
+alias gbDD='git branch --no-color | command grep -vE "^([+*]|\s*($(git_main_branch)|$(git_develop_branch))\s*$)" | command xargs git branch -D 2>/dev/null'
 
 alias npmb='npm run build'
 alias npmbb='npm ci && npm run build'
