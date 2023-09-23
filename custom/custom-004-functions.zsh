ce52f6bb1ee90c21e44d33ed1ffff8083422efa1
Luis Sotomayor <luis@zuub.com>
2023-09-22 21:35:19 -0700

    feat: add variable reference

--- custom/custom-004-functions.zsh
+++ custom/custom-004-functions.zsh
@@ -1,3 +1,3 @@
 function bth () {
-    blueutil --paired | grep 'AirPods 2' | awk '{print $2}' | sed 's/,//' | xargs blueutil --connect
+    blueutil --paired | grep "$BLUETOOTH_DEVICE" | awk '{print $2}' | sed 's/,//' | xargs blueutil --connect
 }
