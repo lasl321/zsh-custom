06a5c9d96e60f9bdd546331c68c3c08723759947
Luis Sotomayor <luis@zuub.com>
2024-03-14 20:11:26 -0700

    feat: update AirPods helpers

--- custom/custom-004-functions.zsh
+++ custom/custom-004-functions.zsh
@@ -1,3 +1,7 @@
-function bth () {
+function connect-airpods () {
     blueutil --paired | grep -e $BLUETOOTH_DEVICE | awk '{print $2}' | sed 's/,//' | xargs blueutil --connect
+}
+
+function disconnect-airpods () {
+    blueutil --paired | grep -e $BLUETOOTH_DEVICE | awk '{print $2}' | sed 's/,//' | xargs blueutil --disconnect
 }
