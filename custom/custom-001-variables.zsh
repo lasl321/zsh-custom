b7234ed4b27dc20cd7cd7f40408bd6d19456d639
Luis Sotomayor <luis@zuub.com>
2025-11-13 20:41:24 -0800

    feat: add pull request template

--- custom/custom-001-variables.zsh
+++ custom/custom-001-variables.zsh
@@ -1,3 +1,6 @@
 BLUETOOTH_DEVICE='AirPods 2'
-EDITOR=nvim
+CODE_HOME=$HOME/code
 PATH=$HOME/.local/bin/:$PATH
+
+export EDITOR=nvim
+export PULL_REQUEST_TEMPLATE=$CODE_HOME/.github/pull-request-template.md
