bc108282498576b181589037c50f3f5a03c3f4fb
Luis Sotomayor <luis.sotomayor@gmail.com>
2025-12-24 19:03:57 -0800

    feat: replace prefix strategy

--- custom/custom-000-initialization.zsh
+++ custom/custom-000-initialization.zsh
@@ -1,5 +1,5 @@
 export NVM_DIR="$HOME/.nvm"
-[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"
+[ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && \. "$(brew --prefix)/opt/nvm/nvm.sh"
 
 source "${0:a:h}/../.p10k.zsh"
 
