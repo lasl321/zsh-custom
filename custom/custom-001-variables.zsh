1d55830d01abbe6314f121441fbb2d78d66d829f
Luis Sotomayor <luis@gleneseo.com>
2026-05-21 14:27:31 -0700

    feat: update environment variables for OpenCode

--- custom/custom-001-variables.zsh
+++ custom/custom-001-variables.zsh
@@ -1,6 +1,8 @@
 BLUETOOTH_DEVICE='AirPods 2'
 CODE_HOME=$HOME/code
 PATH=$HOME/.local/bin/:$PATH
+ZSH_CUSTOM_PATH=$(dirname "${0:a:h}")
 
 export EDITOR=nvim
-export PULL_REQUEST_TEMPLATE=$CODE_HOME/.github/pull-request-template.md
+export OPENCODE_CONFIG="$ZSH_CUSTOM_PATH/opencode.json"
+export OPENCODE_TUI_CONFIG="$ZSH_CUSTOM_PATH/opencode-tui.json"
