c340f3f17824a65ef6ecce636d4071c2e0dc1e2d
Luis Sotomayor <luis@gleneseo.com>
2026-08-22 11:57:50 -0700

    chore: add gcloud

--- custom/custom-000-initialization.zsh
+++ custom/custom-000-initialization.zsh
@@ -13,3 +13,9 @@ for m in visual viopp; do
         bindkey -M $m $c select-quoted
     done
 done
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f "$HOME/Applications/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/Applications/google-cloud-sdk/path.zsh.inc"; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f "$HOME/Applications/google-cloud-sdk/completion.zsh.inc" ]; then . "$HOME/Applications/google-cloud-sdk/completion.zsh.inc"; fi
